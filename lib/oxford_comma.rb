89d56eba4057b1ad87b0c8b8a9e71593c3156b11
Anita Beauchamp <amadamspr@gmail.com>
2018-07-19 05:25:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,13 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   else
-    #some code
+    #Remove last element from array and store in a variable last_element
     last_element = array.pop
+
+    #Convert the array into a string separate by ", "
     string = array.join(", ")
+
+    #Add the last element to the string with an Oxford comma (", and")
     string = string + ", and #{last_element}"
   end
 end
